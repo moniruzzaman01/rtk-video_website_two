69c275d80fcf4c970efccb2adb75b4ff71e12e4c
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-01-20 22:58:59 +0600

    api slice created with all video fetching api and showed all loaded videos on UI

--- src/components/videos/Videos.jsx
+++ src/components/videos/Videos.jsx
@@ -1,15 +1,29 @@
+import { useFetchAllVideosQuery } from "../../api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
+  const { data: videos, isLoading, isError } = useFetchAllVideosQuery();
+
+  let content = null;
+  if (isLoading) {
+    content = (
+      <>
+        {Array(12)
+          .fill(0)
+          .map((a, b) => (
+            <VideoLoader key={b} />
+          ))}
+      </>
     );
+  } else if (isError) {
+    content = <Error />;
+  } else if (videos.length == 0) {
+    content = <Error />;
+  } else if (videos.length > 0) {
+    content = videos.map((video, key) => <Video key={key} video={video} />);
+  }
+
+  return <>{content}</>;
 }
